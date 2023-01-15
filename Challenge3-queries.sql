9b6082c7967ef919c05beaeaae68fcfa7aa1a0d0
youssefbennour <youssefbennour15@gmail.com>
2023-01-15 19:55:15 +0100

    All queries Working

--- Challenge3-queries.sql
+++ Challenge3-queries.sql
@@ -1,6 +1,5 @@
 USE AdventureWorks2019;
-/* Show the first name and the email address of customer with CompanyName 'Bike World' */
---, [store].[Name]
+-- Q1 : Show the first name and the email address of customer with CompanyName 'Bike World' 
 
 SELECT p.FirstName, mail.EmailAddress
 FROM Person.Person p
@@ -12,7 +11,7 @@ INNER JOIN Sales.Store store
 ON  c.StoreID = store.BusinessEntityID
 WHERE store.[Name] = 'Bike World';
 
---Show the CompanyName for all customers with an address in City 'Dallas'.
+-- Q2  : Show the CompanyName for all customers with an address in City 'Dallas'.
 SELECT DISTINCT store.[Name]
 FROM Sales.Store store
 INNER JOIN Sales.Customer c
@@ -24,7 +23,7 @@ ON [address].AddressID = soh.BillToAddressID
 WHERE [address].City = 'Dallas';
 
 
---How many items with ListPrice more than $1000 have been sold?
+--Q3 : How many items with ListPrice more than $1000 have been sold?
 SELECT COUNT(DISTINCT product.ProductID)
 FROM Production.Product product
 JOIN Sales.SalesOrderDetail sod
@@ -32,7 +31,7 @@ ON product.ProductID = sod.ProductID
 WHERE product.ListPrice > 1000;
 
 
---Give the CompanyName of those customers with orders over $100000. Include the subtotal plus tax plus freight.
+--Q4 : Give the CompanyName of those customers with orders over $100000. Include the subtotal plus tax plus freight.
 
 SELECT store.[Name] as CompanyName
 FROM Sales.SalesOrderHeader salesHeader
@@ -45,7 +44,7 @@ GROUP BY  store.Name;
 
 
 
---Find the number of left racing socks ('Racing Socks, L') ordered by CompanyName 'Riding Cycles'
+-- Q5 : Find the number of left racing socks ('Racing Socks, L') ordered by CompanyName 'Riding Cycles'
 SELECT SUM(sod.OrderQty)
 FROM Production.Product pr
 INNER JOIN Sales.SalesOrderDetail sod
@@ -59,17 +58,17 @@ ON c.StoreID = store.BusinessEntityID
 WHERE pr.[Name] = 'Racing Socks, L' and store.[Name] = 'Riding Cycles';
 
 
---A "Single Item Order" is a customer order where only one item is ordered. Show the SalesOrderID and the UnitPrice for every Single Item Order.
+-- Q6 :A "Single Item Order" is a customer order where only one item is ordered. Show the SalesOrderID and the UnitPrice for every Single Item Order.
 
 SELECT Sales.SalesOrderDetail.SalesOrderID, Sales.SalesOrderDetail.UnitPrice
 FROM Sales.SalesOrderDetail
 INNER JOIN Sales.SalesOrderHeader
 ON Sales.SalesOrderDetail.SalesOrderID = Sales.SalesOrderHeader.SalesOrderID
 WHERE Sales.SalesOrderDetail.LineTotal = Sales.SalesOrderHeader.SubTotal;
---HAVING COUNT(Sales.SalesOrderDetail.SalesOrderID) = 1;
+--  Q7 :HAVING COUNT(Sales.SalesOrderDetail.SalesOrderID) = 1;
 
---Where did the racing socks go? List the product name and the CompanyName for all Customers who ordered ProductModel 'Racing Socks'.
-SELECT Production.Product.[Name] ,Sales.Store.[Name]
+-- Q8 : Where did the racing socks go? List the product name and the CompanyName for all Customers who ordered ProductModel 'Racing Socks'.
+SELECT Production.Product.[Name] AS [Product Name] ,Sales.Store.[Name] AS [Company Name]
 FROM Production.ProductModel
 INNER JOIN Production.Product
 ON ProductModel.ProductModelID = Product.ProductModelID
@@ -85,7 +84,7 @@ WHERE ProductModel.[Name] = 'Racing Socks'
 GROUP BY Product.[Name], Store.[Name];
 
 
---Show the product description for culture 'fr' for product with ProductID 736.
+-- Q8 : Show the product description for culture 'fr' for product with ProductID 736.
 SELECT Production.ProductDescription.[Description]
 FROM Production.ProductModelProductDescriptionCulture
 INNER JOIN Production.ProductDescription
@@ -95,7 +94,7 @@ ON Product.ProductModelID = ProductModelProductDescriptionCulture.ProductModelID
 
 WHERE ProductModelProductDescriptionCulture.CultureID = 'fr'  AND Product.ProductID = 736;
 
---Use the SubTotal value in SaleOrderHeader to list orders from the largest to the smallest. For each order show the CompanyName and the SubTotal and the total weight of the order.
+-- Q9 :Use the SubTotal value in SaleOrderHeader to list orders from the largest to the smallest. For each order show the CompanyName and the SubTotal and the total weight of the order.
 
 SELECT Sales.SalesOrderHeader.SubTotal, Store.[Name], CONCAT(SUM(Product.[Weight]*SalesOrderDetail.OrderQty),' ', Product.WeightUnitMeasureCode) AS TotalWeight
 FROM Sales.SalesOrderHeader
@@ -111,7 +110,7 @@ GROUP BY SalesOrderHeader.SalesOrderID, SalesOrderHeader.SubTotal, Store.[Name],
 ORDER BY SalesOrderHeader.SubTotal DESC;
 
 
---How many products in ProductCategory 'Cranksets' have been sold to an address in 'London'?
+-- Q10 : How many products in ProductCategory 'Cranksets' have been sold to an address in 'London'?
 SELECT COUNT(*)
 
 FROM Production.Product
@@ -127,7 +126,7 @@ INNER JOIN Person.[Address]
 ON SalesOrderHeader.ShipToAddressID = [Address].AddressID
 WHERE ProductSubcategory.[Name] = 'Cranksets' ANd [Address].City = 'London';
 
---For every customer witha 'Main Office' in Dallas show AddressLine1 of the 'Main Office' and AddressLine1 of the 'Shipping' address - if there is no shipping address leave it blank. Use one row per customer.
+-- Q11 : For every customer witha 'Main Office' in Dallas show AddressLine1 of the 'Main Office' and AddressLine1 of the 'Shipping' address - if there is no shipping address leave it blank. Use one row per customer.
 WITH mainOffice AS 
 		(SELECT SalesOrderHeader.SalesOrderID , [Address].AddressLine1
 		FROM Sales.SalesOrderHeader
@@ -149,19 +148,19 @@ INNER JOIN Sales.Customer
 ON SalesOrderHeader.CustomerID = Customer.CustomerID
 GROUP BY Customer.CustomerID, [Address].AddressLine1, MainOffice.AddressLine1;
 
---For each order show the SalesOrderID and SubTotal calculated From the SalesOrderHeader
+-- Q12 : For each order show the SalesOrderID and SubTotal calculated From the SalesOrderHeader
 SELECT SalesOrderHeader.SalesOrderID, SalesOrderHeader.SubTotal AS SubTotal
 FROM Sales.SalesOrderHeader;
 
 
---For each order show the SalesOrderID and SubTotal calculated From Sum of OrderQty*UnitPrice
+-- Q13 : For each order show the SalesOrderID and SubTotal calculated From Sum of OrderQty*UnitPrice
 SELECT SalesOrderDetail.SalesOrderID ,SUM(SalesOrderDetail.OrderQty * SalesOrderDetail.UnitPrice) AS SubTotal
 FROM Sales.SalesOrderDetail
 GROUP BY SalesOrderDetail.SalesOrderID;
 
 
 
---For each order show the SalesOrderID and SubTotal calculated From Sum of OrderQty*ListPrice
+-- Q14 : For each order show the SalesOrderID and SubTotal calculated From Sum of OrderQty*ListPrice
 
 SELECT SalesOrderDetail.SalesOrderID, SUM(SalesOrderDetail.OrderQty * Product.ListPrice *(1 -SalesOrderDetail.UnitPriceDiscount)) AS SubTotal
 FROM Sales.SalesOrderDetail
@@ -170,12 +169,7 @@ ON SalesOrderDetail.ProductID = Product.ProductID
 GROUP BY SalesOrderDetail.SalesOrderID;
 
 
---SELECT SpecialOffer.
---FROM Sales.SpecialOffer
-
-----WHERE SalesOrderDetail.SalesOrderID = 43663
-
---Show the best selling item by value
+-- Q15 : Show the best selling item by value
 SELECT TOP(1) Product.[Name] AS [Product Name], CONCAT(SUM(SalesOrderDetail.LineTotal), '$') AS [Total Orders]
 FROM Sales.SalesOrderDetail
 INNER JOIN Production.Product
@@ -184,58 +178,59 @@ GROUP BY Product.[Name]
 ORDER BY SUM(SalesOrderDetail.LineTotal) DESC;
 
 
---Show how many orders are in the following ranges (in $): (0-99)(100-999)(1000-9999)(1000-)
---WITH FilteredRanges AS (
---					SELECT CASE
---								WHEN SalesOrderHeader.TotalDue BETWEEN 0 AND 99 then SUM(SalesOrderHeader.TotalDue) END
-								
---					FROM Sales.SalesOrderHeader
-					
---					)
+-- Q16: Show how many orders are in the following ranges (in $): (0-99)(100-999)(1000-9999)(1000-)
 
+WITH SingleOrdersInRange AS
+			(SELECT	CASE 
+						WHEN SalesOrderHeader.TotalDue BETWEEN 0 AND 99.9999 THEN '0 - 99' 
+						WHEN SalesOrderHeader.TotalDue BETWEEN 100 AND 999.9999 THEN '100 - 999' 
+						WHEN SalesOrderHeader.TotalDue BETWEEN 1000 AND 9999.9999 THEN '1000 - 9999' 
+						WHEN SalesOrderHeader.TotalDue >= 10000  THEN '10000 - ' 
+					END AS [RANGE],
+					CASE 
+						WHEN SalesOrderHeader.TotalDue BETWEEN 0 AND 99.9999 THEN 1
+						WHEN SalesOrderHeader.TotalDue BETWEEN 100 AND 999.9999 THEN 2
+						WHEN SalesOrderHeader.TotalDue BETWEEN 1000 AND 9999.9999 THEN 3
+						WHEN SalesOrderHeader.TotalDue >= 10000  THEN 4
+					END AS [RANK],
+			SalesOrderHeader.TotalDue
+			FROM Sales.SalesOrderHeader)
+SELECT [RANGE], COUNT(*) AS [Num Orders], SUM(SingleOrdersInRange.TotalDue) AS [Total Value]
+FROM SingleOrdersInRange
+GROUP BY [RANGE], [RANK]
+ORDER BY [RANK] ASC;
+	
 
+-- Q17: Identify the three most important cities. Show the break down of top level product category against city.
+WITH CategoryOrderedPerCity AS 
+		(
+			SELECT [Address].City, ProductCategory.[Name] AS CategoryName,
+					COUNT(ProductCategory.[Name]) AS ProductCategoryCount, 
+					RANK() OVER (PARTITION BY [Address].City ORDER BY COUNT(ProductCategory.[Name]) DESC) AS  [RANK]
+			FROM Production.ProductCategory
+			INNER JOIN Production.ProductSubcategory
+			ON ProductCategory.ProductCategoryID = ProductSubcategory.ProductSubcategoryID
+			INNER JOIN Production.Product
+			ON Product.ProductSubcategoryID = ProductSubcategory.ProductSubcategoryID
+			INNER JOIN Sales.SalesOrderDetail
+			ON SalesOrderDetail.ProductID = Product.ProductID
+			INNER JOIN Sales.SalesOrderHeader
+			ON SalesOrderDetail.SalesOrderID = SalesOrderHeader.SalesOrderID
+			INNER JOIN Person.[Address]
+			ON [Address].AddressID = SalesOrderHeader.ShipToAddressID
+			GROUP BY [Address].City, ProductCategory.[Name])
 
---Identify the three most important cities. Show the break down of top level product category against city.
-	WITH BestCategoryPerCity AS (
-									SELECT [Address].City, ProductCategory.[Name] AS CategoryName,COUNT(ProductCategory.[Name]) AS ProductCategoryCount
-									FROM Production.ProductCategory
-									INNER JOIN Production.ProductSubcategory
-									ON ProductCategory.ProductCategoryID = ProductSubcategory.ProductSubcategoryID
-									INNER JOIN Production.Product
-									ON Product.ProductSubcategoryID = ProductSubcategory.ProductSubcategoryID
-									INNER JOIN Sales.SalesOrderDetail
-									ON SalesOrderDetail.ProductID = Product.ProductID
-									INNER JOIN Sales.SalesOrderHeader
-									ON SalesOrderDetail.SalesOrderID = SalesOrderHeader.SalesOrderID
-									INNER JOIN Person.[Address]
-									ON [Address].AddressID = SalesOrderHeader.ShipToAddressID
-									GROUP BY [Address].City, ProductCategory.[Name]
-								
-								)
-
-
-	SELECT TOP(3)  [Address].City, CONCAT(SUM(SalesOrderHeader.TotalDue),'$'), BestCategoryPerCity.ProductCategoryCount
-	FROM Sales.SalesOrderHeader
-	INNER JOIN Person.[Address]
-	ON [Address].AddressID = SalesOrderHeader.ShipToAddressID
-	INNER JOIN BestCategoryPerCity
-	ON BestCategoryPerCity.City = [Address].City
-	GROUP BY [Address].City, BestCategoryPerCity.ProductCategoryCount
-	ORDER BY SUM(SalesOrderHeader.TotalDue) DESC, BestCategoryPerCity.ProductCategoryCount DESC
-
-
-
-
-
-SELECT TOP(3) WITH TIES  [Address].City, CONCAT(SUM(SalesOrderHeader.TotalDue),'$')
+SELECT TOP(3)  [Address].City, CONCAT(SUM(SalesOrderHeader.TotalDue),'$') AS [Total Value Of Orders], CategoryOrderedPerCity.ProductCategoryCount
 FROM Sales.SalesOrderHeader
 INNER JOIN Person.[Address]
 ON [Address].AddressID = SalesOrderHeader.ShipToAddressID
+INNER JOIN CategoryOrderedPerCity
+ON CategoryOrderedPerCity.City = [Address].City
+WHERE CategoryOrderedPerCity.[RANK] = 1
+GROUP BY [Address].City, CategoryOrderedPerCity.ProductCategoryCount
+ORDER BY SUM(SalesOrderHeader.TotalDue) DESC, CategoryOrderedPerCity.ProductCategoryCount DESC;
 
-GROUP BY [Address].City
-ORDER BY SUM(SalesOrderHeader.TotalDue) DESC
-
---List the SalesOrderNumber for the customer 'Good Toys' 'Bike World'
+-- Q18 : List the SalesOrderNumber for the customer 'Good Toys' 'Bike World'
 SELECT SalesOrderHeader.SalesOrderID, Store.[Name]
 FROM Sales.SalesOrderHeader
 INNER JOIN Sales.Customer
@@ -244,7 +239,7 @@ INNER JOIN Sales.Store
 ON Store.BusinessEntityID = Customer.StoreID
 WHERE Store.[Name] = 'Good Toys' OR Store.[Name] = 'Bike World'
 
---List the ProductName and the quantity of what was ordered by 'Futuristic Bikes'
+-- Q19 : List the ProductName and the quantity of what was ordered by 'Futuristic Bikes'
 SELECT Product.[Name], SUM(SalesOrderDetail.OrderQty) AS [Order quantity]
 FROM Sales.Store
 INNER JOIN Sales.Customer
@@ -256,10 +251,34 @@ ON SalesOrderDetail.SalesOrderID = SalesOrderHeader.SalesOrderID
 INNER JOIN Production.Product
 ON Product.ProductID = SalesOrderDetail.ProductID
 WHERE Store.[Name] =  'Futuristic Bikes'
-GROUP BY Product.[Name]
+GROUP BY Product.[Name];
 
---Show the total order value for each CountryRegion. List by value with the highest first.
-SELECT CountryRegion.[Name], COUNT(SalesOrderHeader.SalesOrderID) as [Total Orders]
+-- Q20 : List the name and addresses of companies containing the word 'Bike' (upper or lower case) and companies containing 'cycle' (upper or lower case). Ensure that the 'bike's are listed before the 'cycles's.
+
+WITH BikesAndCyclesAddresses AS(
+			SELECT Store.[Name],
+			AddressLine1,
+			AddressLine2,
+			City,
+			StateProvinceName,
+			PostalCode, 
+			CountryRegionName, 
+			CASE 
+				WHEN LOWER(Store.Name) LIKE '%bike%' THEN  1
+				WHEN LOWER(Store.Name) LIKE '%cycle%' THEN 2
+			END AS [Rank]
+			FROM Sales.Store
+			INNER JOIN Sales.vStoreWithAddresses
+			ON Store.[Name] = vStoreWithAddresses.[Name]
+			WHERE LOWER(Store.[Name]) LIKE  '%bike%' OR LOWER(Store.[Name]) LIKE '%cycle%'
+		)
+SELECT [Name] AS [Company Name], AddressLine1, AddressLine2, City, StateProvinceName, PostalCode, CountryRegionName
+FROM BikesAndCyclesAddresses
+ORDER BY [Rank];
+
+
+-- Q21: Show the total order value for each CountryRegion. List by value with the highest first.
+SELECT CountryRegion.[Name], CONCAT(SUM(SalesOrderHeader.TotalDue),'$') as [Total Orders]
 FROM Sales.SalesOrderHeader
 INNER JOIN Person.[Address] 
 ON SalesOrderHeader.ShipToAddressID = [Address].AddressID
@@ -267,18 +286,31 @@ INNER JOIN Person.StateProvince
 ON StateProvince.StateProvinceID = [Address].StateProvinceID
 INNER JOIN Person.CountryRegion
 ON CountryRegion.CountryRegionCode = StateProvince.CountryRegionCode
-GROUP BY CountryRegion.[Name]
+GROUP BY CountryRegion.[Name];
 
---Find the best customer in each region.
-SELECT CountryRegion.[Name],Customer.CustomerID, SUM(SalesOrderHeader.TotalDue)AS [Total Ordered Value]
-FROM  Sales.SalesOrderHeader
-INNER JOIN Sales.Customer
-ON Customer.CustomerID = SalesOrderHeader.CustomerID
-INNER JOIN Person.[Address]
-ON [Address].AddressID = SalesOrderHeader.BillToAddressID
-INNER JOIN Person.StateProvince
-ON StateProvince.StateProvinceID = [Address].StateProvinceID
-INNER JOIN Person.CountryRegion
-ON StateProvince.CountryRegionCode = CountryRegion.CountryRegionCode
-GROUP BY CountryRegion.[Name], Customer.CustomerID
-ORDER BY [Total Ordered Value] DESC
+--Q22 : Find the best customer in each region.
+
+
+WITH RegionOrderedCustomers AS (
+		SELECT	CountryRegion.[Name],
+				Customer.CustomerID, 
+				SUM(SalesOrderHeader.TotalDue)AS [Total Ordered Value], 
+				RANK() OVER (PARTITION BY CountryRegion.[Name] ORDER BY  SUM(SalesOrderHeader.TotalDue) DESC) AS [RANK]
+		FROM  Sales.SalesOrderHeader
+		INNER JOIN Sales.Customer
+		ON Customer.CustomerID = SalesOrderHeader.CustomerID
+		INNER JOIN Person.[Address]
+		ON [Address].AddressID = SalesOrderHeader.BillToAddressID
+		INNER JOIN Person.StateProvince
+		ON StateProvince.StateProvinceID = [Address].StateProvinceID
+		INNER JOIN Person.CountryRegion
+		ON StateProvince.CountryRegionCode = CountryRegion.CountryRegionCode
+		GROUP BY CountryRegion.[Name], Customer.CustomerID)
+
+SELECT RegionOrderedCustomers.[Name], RegionOrderedCustomers.CustomerID, RegionOrderedCustomers.[Total Ordered Value]
+FROM RegionOrderedCustomers
+WHERE RegionOrderedCustomers.[RANK] = 1
+ORDER BY RegionOrderedCustomers.[Total Ordered Value] DESC
+
+
+
